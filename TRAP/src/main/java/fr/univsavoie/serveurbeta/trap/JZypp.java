1a85ab80281f297a554003e80211258ecff1ed86
patrick-edouard <proux@etu.univ-savoie.fr>
2014-03-27 10:24:52 +0100

    Test avec le jar:file:

--- TRAP/src/main/java/fr/univsavoie/serveurbeta/trap/JZypp.java
+++ TRAP/src/main/java/fr/univsavoie/serveurbeta/trap/JZypp.java
@@ -1,6 +1,9 @@
 package fr.univsavoie.serveurbeta.trap;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 class JZypp{
 
@@ -9,8 +12,18 @@ class JZypp{
         File lib = new File("TRAP/resources/libZyppImpl.so"); //local developpement
 
         System.out.println(lib);
-        if(!lib.exists())
-            lib = new File("TRAP-1.0-SNAPSHOT.jar!"+"/libZyppImpl.so"); //For jar
+
+        if(!lib.exists()){
+            try{
+                lib = new File(new URL("jar:file:\"TRAP-1.0-SNAPSHOT.jar!/libZyppImpl.so\"").toURI());
+            }catch(URISyntaxException e){
+                e.printStackTrace();
+            }catch(MalformedURLException e2){
+                e2.printStackTrace();
+            }
+
+        }
+
         //Convert the relative path to absolute
         System.load((lib.getAbsolutePath()));
 
